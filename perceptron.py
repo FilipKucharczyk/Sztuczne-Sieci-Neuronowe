041c759623636492ffca9aed28e2f6ea7ee35bce
Filip <01197495@pw.edu.pl>
2026-01-07 17:04:33 +0100

    updated charts

--- perceptron.py
+++ perceptron.py
@@ -49,31 +49,26 @@ class DlNet:
         # Wymiar: HIDDEN_L_SIZE wejść x 1 wyjście
         self.W2 = np.random.randn(self.HIDDEN_L_SIZE, 1)
         self.b2 = np.zeros((1, 1))
-        
-        # Zmienne pomocnicze do przechowywania stanów pośrednich (dla backprop)
+        # Zmienne pomocnicze do przechowywania stanów pośrednich 
         self.hidden_input = None
         self.hidden_output = None
-#ToDo        
 
-    
     def forward(self, x):  
-        # 1. Obliczenie wejścia do warstwy ukrytej: z = x * W1 + b1
         # Reshape x na (1,1) aby zgadzały się wymiary macierzy
         x_in = x.reshape(1, 1)
         
+        # Obliczenie wejścia do warstwy ukrytej: z = x * W1 + b1
         self.hidden_input = np.dot(x_in, self.W1) + self.b1
         
-        # 2. Funkcja aktywacji warstwy ukrytej
+        # Funkcja aktywacji warstwy ukrytej
         self.hidden_output = sigmoid(self.hidden_input)
         
-        # 3. Warstwa wyjściowa (liniowa): y = a * W2 + b2
+        # Warstwa wyjściowa  y = a * W2 + b2
         self.y_out = np.dot(self.hidden_output, self.W2) + self.b2
         
-        return self.y_out[0][0] # Zwracamy skalar
-#ToDo        
+        return self.y_out[0][0] # Zwracamy skalar      
         
     def predict(self, x):    
-       # ToDo: Predykcja to po prostu forward pass, ale dla wektora danych
         # Obliczamy wynik dla każdego x w tablicy wejściowej
         y_pred = []
         for val in x:
@@ -81,65 +76,55 @@ class DlNet:
             y_pred.append(res)
         return np.array(y_pred)
         
-    def backward(self, x, y):
-        # ToDo: Propagacja wsteczna (Slajdy 18-19)
-        
+    def backward(self, x, y): 
         # Przygotowanie danych (reshape dla macierzy)
         x_in = x.reshape(1, 1)
         y_target = np.array([[y]])
         
-        # 1. Pochodna funkcji straty po wyjściu sieci (dy_out)
-        diff = d_nloss(self.y_out, y_target) # Skalar (lub macierz 1x1)
+        #błąd na wyjściu
+        diff = d_nloss(self.y_out, y_target)
         
-        # 2. Gradienty dla warstwy wyjściowej (Slajd 18)
+        # Gradienty dla warstwy wyjściowej
         # dW2 = wyjście_ukrytej * błąd
         grad_W2 = np.dot(self.hidden_output.T, diff)
         grad_b2 = diff
         
-        # 3. Propagacja błędu do warstwy ukrytej (Slajd 19)
-        # błąd_w_ukrytej = błąd_wyjścia * W2_transponowane
+        # błąd warstwy ukrytej 
+        # błąd_ukrytej = błąd_wyjścia * W2_transponowane
         error_hidden = np.dot(diff, self.W2.T)
         
-        # 4. Pochodna funkcji aktywacji (sigmoid)
-        # delta_hidden = błąd_w_ukrytej * pochodna_sigmoid(wejście_do_ukrytej)
+        # delta_hidden = błąd_ukrytej * pochodna_sigmoid(wejście_do_ukrytej)
         delta_hidden = error_hidden * d_sigmoid(self.hidden_input)
         
-        # 5. Gradienty dla warstwy ukrytej
+        # Gradienty dla warstwy ukrytej
         grad_W1 = np.dot(x_in.T, delta_hidden)
         grad_b1 = delta_hidden
         
-        # 6. Aktualizacja wag (Metoda gradientu prostego)
+        # Aktualizacja wag 
         self.W2 -= self.LR * grad_W2
         self.b2 -= self.LR * grad_b2
         self.W1 -= self.LR * grad_W1
         self.b1 -= self.LR * grad_b1
-#ToDo        
         
     def train(self, x_set, y_set, iters):    
         for i in range(0, iters):
-            # ToDo: Pętla uczenia (Slajd 29 - SGD)
-            # 1. Losowanie próbki (Stochastic Gradient Descent)
+            # Losowanie próbki
             idx = np.random.randint(0, len(x_set))
             x_sample = x_set[idx]
             y_sample = y_set[idx]
-            
-            # 2. Forward pass
             self.forward(np.array([x_sample]))
-            
-            # 3. Backward pass (obliczenie gradientów i aktualizacja wag)
             self.backward(np.array([x_sample]), y_sample)
-#ToDo                
-
-        
+          
 nn = DlNet(x,y)
 nn.train(x, y, 15000)
-
 yh = nn.predict(x) #ToDo tu umiesciÄ wyniki (y) z sieci
 
+# Obliczanie błędów MSE i MAE
+mse = np.mean((y - yh) ** 2)
+mae = np.mean(np.abs(y - yh))
 import matplotlib.pyplot as plt
 
-
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(1, 1, 1)
 ax.spines['left'].set_position('center')
 ax.spines['bottom'].set_position('zero')
@@ -148,7 +133,17 @@ ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
-plt.plot(x,y, 'r')
-plt.plot(x,yh, 'b')
+plt.plot(x, y, 'r',  label='Funkcja celu J(x)')
+plt.plot(x, yh, 'b',  label='Aproksymacja sieci')
+plt.legend(loc='upper right')
+plt.title(f"Liczba neuronów ukrytych: {nn.HIDDEN_L_SIZE}", fontsize=10)
 
+textstr = '\n'.join((
+    f'MSE: {mse:.4f}',
+    f'MAE: {mae:.4f}'))
+props = dict(alpha=0.5)
+ax.text(0.05, 0.01, textstr, transform=ax.transAxes, fontsize=12,
+        verticalalignment='top', bbox=props)
+
+plt.grid(True)
 plt.show()
